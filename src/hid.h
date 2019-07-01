62dc893663a1c71ae0a76140d11cce9093b3d21c
jatin <j@redballoonsecurity.com>
2019-07-01 15:57:29 -0400

    fixing tabs

--- src/hid.h
+++ src/hid.h
@@ -76,13 +76,13 @@ e-mail   :  support@circuitsathome.com
 #define HID_REQUEST_SET_PROTOCOL    0x0B
 
 /* Class Descriptor Types */
-#define HID_DESCRIPTOR_HID			0x21
-#define HID_DESCRIPTOR_REPORT		0x22
-#define HID_DESRIPTOR_PHY			0x23
+#define HID_DESCRIPTOR_HID          0x21
+#define HID_DESCRIPTOR_REPORT       0x22
+#define HID_DESRIPTOR_PHY           0x23
 
 /* Protocol Selection */
-#define HID_BOOT_PROTOCOL			0x00
-#define HID_RPT_PROTOCOL			0x01
+#define HID_BOOT_PROTOCOL           0x00
+#define HID_RPT_PROTOCOL            0x01
 
 /* HID Interface Class Code */
 #define HID_INTF                    0x03
@@ -98,49 +98,49 @@ e-mail   :  support@circuitsathome.com
 /**
  * \brief HidItemPrefix definition.
  */
-struct HidItemPrefix				// Not used
+struct HidItemPrefix                // Not used
 {
-	uint8_t		bSize : 2;
-	uint8_t		bType : 2;
-	uint8_t		bTag  : 4;
+    uint8_t     bSize : 2;
+    uint8_t     bType : 2;
+    uint8_t     bTag  : 4;
 };
 
-#define HID_ITEM_TYPE_MAIN							0
-#define HID_ITEM_TYPE_GLOBAL						1
-#define HID_ITEM_TYPE_LOCAL							2
-#define HID_ITEM_TYPE_RESERVED						3
+#define HID_ITEM_TYPE_MAIN                          0
+#define HID_ITEM_TYPE_GLOBAL                        1
+#define HID_ITEM_TYPE_LOCAL                         2
+#define HID_ITEM_TYPE_RESERVED                      3
 
-#define HID_LONG_ITEM_PREFIX						0xfe		// Long item prefix value
+#define HID_LONG_ITEM_PREFIX                        0xfe        // Long item prefix value
 
-#define bmHID_MAIN_ITEM_TAG							0xfc		// Main item tag mask
+#define bmHID_MAIN_ITEM_TAG                         0xfc        // Main item tag mask
 
-#define bmHID_MAIN_ITEM_INPUT						0x80		// Main item Input tag value
-#define bmHID_MAIN_ITEM_OUTPUT						0x90		// Main item Output tag value
-#define bmHID_MAIN_ITEM_FEATURE						0xb0		// Main item Feature tag value
-#define bmHID_MAIN_ITEM_COLLECTION					0xa0		// Main item Collection tag value
-#define bmHID_MAIN_ITEM_END_COLLECTION				0xce		// Main item End Collection tag value
+#define bmHID_MAIN_ITEM_INPUT                       0x80        // Main item Input tag value
+#define bmHID_MAIN_ITEM_OUTPUT                      0x90        // Main item Output tag value
+#define bmHID_MAIN_ITEM_FEATURE                     0xb0        // Main item Feature tag value
+#define bmHID_MAIN_ITEM_COLLECTION                  0xa0        // Main item Collection tag value
+#define bmHID_MAIN_ITEM_END_COLLECTION              0xce        // Main item End Collection tag value
 
-#define HID_MAIN_ITEM_COLLECTION_PHYSICAL			0
-#define HID_MAIN_ITEM_COLLECTION_APPLICATION		1
-#define HID_MAIN_ITEM_COLLECTION_LOGICAL			2
-#define HID_MAIN_ITEM_COLLECTION_REPORT				3
-#define HID_MAIN_ITEM_COLLECTION_NAMED_ARRAY		4
-#define HID_MAIN_ITEM_COLLECTION_USAGE_SWITCH		5
-#define HID_MAIN_ITEM_COLLECTION_USAGE_MODIFIER		6
+#define HID_MAIN_ITEM_COLLECTION_PHYSICAL           0
+#define HID_MAIN_ITEM_COLLECTION_APPLICATION        1
+#define HID_MAIN_ITEM_COLLECTION_LOGICAL            2
+#define HID_MAIN_ITEM_COLLECTION_REPORT             3
+#define HID_MAIN_ITEM_COLLECTION_NAMED_ARRAY        4
+#define HID_MAIN_ITEM_COLLECTION_USAGE_SWITCH       5
+#define HID_MAIN_ITEM_COLLECTION_USAGE_MODIFIER     6
 
 /**
  * \brief MainItemIOFeature definition.
  */
-struct MainItemIOFeature			// Not used
+struct MainItemIOFeature            // Not used
 {
-	uint8_t		bmIsConstantOrData			: 1;
-	uint8_t		bmIsArrayOrVariable			: 1;
-	uint8_t		bmIsRelativeOrAbsolute		: 1;
-	uint8_t		bmIsWrapOrNoWrap			: 1;
-	uint8_t		bmIsNonLonearOrLinear		: 1;
-	uint8_t		bmIsNoPreferedOrPrefered	: 1;
-	uint8_t		bmIsNullOrNoNull			: 1;
-	uint8_t		bmIsVolatileOrNonVolatile	: 1;
+    uint8_t     bmIsConstantOrData          : 1;
+    uint8_t     bmIsArrayOrVariable         : 1;
+    uint8_t     bmIsRelativeOrAbsolute      : 1;
+    uint8_t     bmIsWrapOrNoWrap            : 1;
+    uint8_t     bmIsNonLonearOrLinear       : 1;
+    uint8_t     bmIsNoPreferedOrPrefered    : 1;
+    uint8_t     bmIsNullOrNoNull            : 1;
+    uint8_t     bmIsVolatileOrNonVolatile   : 1;
 };
 
 class HID;
@@ -153,11 +153,11 @@ class HID;
 class HIDReportParser
 {
 public:
-	virtual void Parse(HID *hid, bool is_rpt_id, uint32_t len, uint8_t *buf) = 0;
+    virtual void Parse(HID *hid, bool is_rpt_id, uint32_t len, uint8_t *buf) = 0;
 };
 
-#define MAX_REPORT_PARSERS					2
-#define HID_MAX_HID_CLASS_DESCRIPTORS		5
+#define MAX_REPORT_PARSERS                  2
+#define HID_MAX_HID_CLASS_DESCRIPTORS       5
 
 /**
  * \class HID definition.
@@ -165,29 +165,29 @@ public:
 class HID : public USBDeviceConfig, public UsbConfigXtracter
 {
 protected:
-	USBHost		*pUsb;					// USB class instance pointer
-	uint32_t	bAddress;				// address
+    USBHost     *pUsb;                  // USB class instance pointer
+    uint32_t    bAddress;               // address
 
 protected:
-	static const uint32_t	epInterruptInIndex	= 1;	// InterruptIN  endpoint index
-	static const uint32_t	epInterruptOutIndex	= 2;	// InterruptOUT endpoint index
+    static const uint32_t   epInterruptInIndex  = 1;    // InterruptIN  endpoint index
+    static const uint32_t   epInterruptOutIndex = 2;    // InterruptOUT endpoint index
 
-	static const uint32_t	maxHidInterfaces	= 3;
-	static const uint32_t	maxEpPerInterface	= 2;
-	static const uint32_t	totalEndpoints		= (maxHidInterfaces * maxEpPerInterface + 1);
+    static const uint32_t   maxHidInterfaces    = 3;
+    static const uint32_t   maxEpPerInterface   = 2;
+    static const uint32_t   totalEndpoints      = (maxHidInterfaces * maxEpPerInterface + 1);
 
-	void PrintEndpointDescriptor(const USB_ENDPOINT_DESCRIPTOR* ep_ptr);
-	void PrintHidDescriptor(const USB_HID_DESCRIPTOR *pDesc);
+    void PrintEndpointDescriptor(const USB_ENDPOINT_DESCRIPTOR* ep_ptr);
+    void PrintHidDescriptor(const USB_HID_DESCRIPTOR *pDesc);
 
-	virtual HIDReportParser* GetReportParser(uint32_t id) { return 0; };
+    virtual HIDReportParser* GetReportParser(uint32_t id) { return 0; };
 
 public:
-	HID(USBHost *pusb) : pUsb(pusb) {};
+    HID(USBHost *pusb) : pUsb(pusb) {};
 
-	const USBHost* GetUsb() { return pUsb; };
-	virtual bool SetReportParser(uint32_t id, HIDReportParser *prs) { return false; };
+    const USBHost* GetUsb() { return pUsb; };
+    virtual bool SetReportParser(uint32_t id, HIDReportParser *prs) { return false; };
 
-	uint32_t SetProtocol(uint32_t iface, uint32_t protocol);
+    uint32_t SetProtocol(uint32_t iface, uint32_t protocol);
     uint32_t GetProtocol(uint32_t iface, uint8_t* dataptr);
     uint32_t GetIdle(uint32_t iface, uint32_t reportID, uint8_t* dataptr);
     uint32_t SetIdle(uint32_t iface, uint32_t reportID, uint32_t duration);
